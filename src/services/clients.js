1c6f4fa20d2e46931c7a23ebfe1ff4a9ebfce6d8
Antonio Martinez <antoniodmartinezp03@gmail.com>
2025-03-13 01:59:24 -0400

    feat/posts and updates for clients

--- src/services/clients.js
+++ src/services/clients.js
@@ -2,6 +2,8 @@ import {
   clientExists,
   getClientsFromSupabase,
   deleteClientsFromSupabase,
+  createClient,
+  updateClient,
 } from "../models/clients.js";
 
 // GET ALL CLIENTS
@@ -45,3 +47,36 @@ export const deleteOneClientService = async (id) => {
     throw new Error('An error occurred: ' + error.message);
   }
 };
+
+export const createClientService = async (clientData) => {
+  try {
+    if (!clientData || Object.keys(clientData).length === 0) {
+      throw new Error('Client data is required');
+    }
+
+    const newClient = await createClient(clientData);
+
+    if (!newClient) {
+      throw new Error('Failed to create client');
+    }
+
+    return newClient;
+  } catch (error) {
+    console.error('Error in createClientService:', error);
+    throw new Error('An error occurred: ' + error.message);
+  }
+};
+
+export const updateClientService = async (id, clientData) => {
+  try {
+    const updatedClient = await updateClient(id, clientData);
+    if (!updatedClient) {
+      throw new Error("Failed to update client");
+    }
+    return updatedClient;
+  } catch (error) {
+    console.error("Error in updateClientService:", error);
+    throw new Error("An error occurred: " + error.message);
+  }
+};
+
